b688ecebc473044b67dd3cbbd26e32f316f4dc1d
123456789-mc <mmendezm21@curnvirtual.edu.co>
2024-11-19 21:25:04 -0500

    Borrado pymsql2.

--- basedatosprueba/settings.py
+++ basedatosprueba/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mi_app',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
