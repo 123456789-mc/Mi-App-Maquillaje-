21f801fe76cbeb344a00eefc1885734ac883478c
123456789-mc <mmendezm21@curnvirtual.edu.co>
2024-11-19 21:46:21 -0500

    Preparando despliegue

--- basedatosprueba/settings.py
+++ basedatosprueba/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-3_f9$k*=%ah$z2*p4-r)4yk=!yo^&()fsz1z%y&qad*0g17!l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # Hosts permitidos
+ALLOWED_HOSTS = ['*']  # O especifica el dominio de Render
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
