33221f41a27a8ea1cc9752525dd0e1fb1edb0c22
123456789-mc <mmendezm21@curnvirtual.edu.co>
2024-11-19 22:08:12 -0500

    modificando el metodo de inicializacion para evitar consultas en Ready

--- mi_app/apps.py
+++ mi_app/apps.py
@@ -1,16 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-
 
 class MiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mi_app'
 
     def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='mmendezm21@curnvirtual.edu.co',
-                password='admin'
-            )
+        post_migrate.connect(create_superuser, sender=self)
+
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='mmendezm21@curnvirtual.edu.co',
+            password='admin'
+        )
