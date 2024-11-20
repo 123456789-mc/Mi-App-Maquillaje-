841e677bb35122e2bf4d310d9e0a12f1d0abcd28
123456789-mc <mmendezm21@curnvirtual.edu.co>
2024-11-19 23:06:31 -0500

    Preparando despliegue

--- basedatosprueba/settings.py
+++ basedatosprueba/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -29,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*.render.com']  # Reemplaza esto por tu dominio de Render o los dominios que permitirás
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
@@ -38,6 +40,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # Esto crea una carpeta llamada 'staticf
 STATICFILES_DIRS = [
     BASE_DIR / 'mi_app/static',  # Cambia 'mi_app' por el nombre de tu aplicación
 ]
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Aquí está la configuración para imágenes:
 MEDIA_URL = '/media/'
