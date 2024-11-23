4779f9e23e3f4fbf0c135e8bfdf9cd0f42c922c3
123456789-mc <mmendezm21@curnvirtual.edu.co>
2024-11-23 12:37:21 -0500

    Borrado pymsql2.

--- mi_app/views.py
+++ mi_app/views.py
@@ -59,7 +59,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('registro.html')  
+                return redirect('inicio.html')  
             else:
                 error = "Usuario o contraseña incorrectos."
         else:
